dc70b2a797814dfdc1798d284b9e44c4c9fae146
Kirschke95 <tylerkirschke95@gmail.com>
2025-02-03 14:12:24 -0500

    finished report function

--- main.py
+++ main.py
@@ -5,10 +5,15 @@ def main():
     word_count = count_words(text)
     #print(word_count)
 
-    character_counts = count_characters(text)
+    character_counts_dict = count_characters(text)
     #print(character_counts)
 
-    print_report(character_counts, book_path, word_count)
+    character_counts_list = dict_to_list(character_counts_dict)
+    character_counts_list.sort(reverse=True, key=sort_on)
+    print_report(character_counts_list, book_path, word_count)
+
+def sort_on(dict):
+    return dict["num"]
 
 def get_book_text(path):
     with open(path) as f:
@@ -32,13 +37,27 @@ def count_characters(text):
             character_counts[char] = 1
     return character_counts
 
+def dict_to_list(char_dict):
+    char_list = []
+
+    #loop through dictionary items
+    for char, count in char_dict.items():
+        #create dictionary for this character and add it to the list
+        char_dict = {"char": char, "num": count}
+        char_list.append(char_dict)
+
+    return char_list
+
 def print_report(characters, path, word_count):
     print(f"--- Begin report of {path} ---")
     print(f"{word_count} words found in the document")
     print()
 
     for character in characters:
-        print(f"The '{character}' was found {characters[character]} times.")
+        if character["char"].isalpha():
+            print(f"The '{character["char"]}' was found {character["num"]} times.")
+        
+    print("--- End report ---")
 
     
 
